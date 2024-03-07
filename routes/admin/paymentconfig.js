66781a7178b0b0c7b7d44e87a907c6871e8c3007
ankit <iankitkhatrani@gmail.com>
2024-03-07 15:48:26 +0530

    add p[ayment config

--- routes/admin/paymentconfig.js
+++ routes/admin/paymentconfig.js
@@ -99,4 +99,93 @@ router.put('/paymentconfigset', async (req, res) => {
 });
 
 
+//==============================
+
+
+/**
+* @api {get} /admin/Getwithdrawconfig
+* @apiName  Getwithdrawconfig
+* @apiGroup  Admin
+* @apiHeader {String}  x-access-token Admin's unique access-key
+* @apiSuccess (Success 200) {Array} badges Array of badges document
+* @apiError (Error 4xx) {String} message Validation or error message.
+*/
+router.get('/Getwithdrawconfig', async (req, res) => {
+    try {
+        console.info('requet => ', req.query);
+        console.info('GAMELOGICCONFIG => ', GAMELOGICCONFIG);
+
+        res.json({
+            autopay: GAMELOGICCONFIG.autopay,
+            autopaymaxlimit: GAMELOGICCONFIG.autopaymaxlimit,
+            minimumwithdrawalamount: GAMELOGICCONFIG.minimumwithdrawalamount,
+            dailywithdrawallimit: GAMELOGICCONFIG.dailywithdrawallimit,
+            dailytransactionlimit: GAMELOGICCONFIG.dailytransactionlimit
+        });
+
+
+      
+
+    } catch (error) {
+        logger.error('admin/dahboard.js post bet-list error => ', error);
+        res.status(config.INTERNAL_SERVER_ERROR).json(error);
+    }
+});
+
+
+/**
+* @api {get} /admin/withdrawconfigset
+* @apiName  withdrawconfigset
+* @apiGroup  Admin
+* @apiHeader {String}  x-access-token Admin's unique access-key
+* @apiSuccess (Success 200) {Array} badges Array of badges document
+* @apiError (Error 4xx) {String} message Validation or error message.
+*/
+router.put('/withdrawconfigset', async (req, res) => {
+    try {
+        console.info('withdrawconfigset requet => ', req.body);
+
+        console.log("req.body.game.gamename  1", req.body)
+
+        // payingateway: GAMELOGICCONFIG.payingateway,
+        // payoutgateway:GAMELOGICCONFIG.payoutgateway,
+        // depositbonus:GAMELOGICCONFIG.depositbonus,
+        // depositbonusinr:GAMELOGICCONFIG.depositbonusinr,
+        // minimumdepositinr:GAMELOGICCONFIG.minimumdepositinr
+
+        if (req.body.autopay != undefined
+            && req.body.autopaymaxlimit != undefined
+            && req.body.minimumwithdrawalamount != undefined
+            && req.body.dailywithdrawallimit != undefined
+            && req.body.dailytransactionlimit != undefined
+        ) {
+            GAMELOGICCONFIG.autopay = req.body.autopay
+            GAMELOGICCONFIG.autopaymaxlimit = req.body.autopaymaxlimit
+            GAMELOGICCONFIG.minimumwithdrawalamount = parseInt(req.body.minimumwithdrawalamount)
+            GAMELOGICCONFIG.dailywithdrawallimit = parseInt(req.body.dailywithdrawallimit)
+            GAMELOGICCONFIG.dailytransactionlimit = parseInt(req.body.dailytransactionlimit)
+
+
+            console.log("GAMELOGICCONFIG ", GAMELOGICCONFIG)
+            let link = "./gamelogic.json"
+            console.log("link ", link)
+            fs.writeFile(link, JSON.stringify(GAMELOGICCONFIG), function (err) {
+                console.log("erre", err)
+                if (err) {
+                    console.log(err);
+                }
+
+            });
+            res.json({ falgs: true });
+        } else {
+            res.json({ falgs: false });
+        }
+
+
+    } catch (error) {
+        logger.error('admin/dahboard.js post bet-list error => ', error);
+        res.status(config.INTERNAL_SERVER_ERROR).json(error);
+    }
+});
+
 module.exports = router;
