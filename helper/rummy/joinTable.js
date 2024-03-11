ddf29192524ac28fa7664036428cd2a6a3821aa1
ankit <iankitkhatrani@gmail.com>
2024-03-11 15:04:59 +0530

    update jointable

--- helper/rummy/joinTable.js
+++ helper/rummy/joinTable.js
@@ -28,9 +28,9 @@ module.exports.joinTable = async (requestData, socket) => {
       return false;
     }
 
-    // if (typeof socket.JT !== 'undefined' && socket.JT) {
-    //   return false;
-    // }
+    if (typeof socket.JT !== 'undefined' && socket.JT) {
+      return false;
+    }
 
     socket.JT = true;
 
