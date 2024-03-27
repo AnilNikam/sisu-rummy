42a4e6eb4804496ffec0d4185263884566ddf4bd
ankit <iankitkhatrani@gmail.com>
2024-03-27 19:51:46 +0530

    push

--- routes/admin/userhistory.js
+++ routes/admin/userhistory.js
@@ -19,54 +19,11 @@ const GameHistory = mongoose.model("tableHistory");
 * @apiSuccess (Success 200) {Array} badges Array of badges document
 * @apiError (Error 4xx) {String} message Validation or error message.
 */
-router.get('/BackandWhiteHistory', async (req, res) => {
+router.get('/rummyHistory', async (req, res) => {
     try {
         console.info('requet => ', req.query);
 
-        // const BlackandWhiteData =  [
-        //     {
-        //         "SrNo": 1,
-        //         "DateTime": "2023-10-10 08:30 AM",
-        //         "Name": "Alice",
-        //         "PhoneNumber": "123-456-7890",
-        //         "RoomId": "RHRoom1",
-        //         "Amount": 100, // Amount in this example (can be credit or debit)
-        //         "Type": "Credit", // "Credit" or "Debit"
-        //         "Club": "Club A"
-        //     },
-        //     {
-        //         "SrNo": 2,
-        //         "DateTime": "2023-10-09 10:15 AM",
-        //         "Name": "Bob",
-        //         "PhoneNumber": "987-654-3210",
-        //         "RoomId": "RHRoom2",
-        //         "Amount": 50, // Amount in this example (can be credit or debit)
-        //         "Type": "Debit", // "Credit" or "Debit"
-        //         "Club": "Club B"
-        //     },
-        //     {
-        //         "SrNo": 3,
-        //         "DateTime": "2023-10-09 10:15 AM",
-        //         "Name": "Bob",
-        //         "PhoneNumber": "987-654-3210",
-        //         "RoomId": "RHRoom2",
-        //         "Amount": 50, // Amount in this example (can be credit or debit)
-        //         "Type": "Debit", // "Credit" or "Debit"
-        //         "Club": "Club Bd"
-        //     }, {
-        //         "SrNo": 3,
-        //         "DateTime": "2023-10-09 10:15 AM",
-        //         "Name": "Bob",
-        //         "PhoneNumber": "987-654-3210",
-        //         "RoomId": "RHRoom2",
-        //         "Amount": 50, // Amount in this example (can be credit or debit)
-        //         "Type": "Debit", // "Credit" or "Debit"
-        //         "Club": "Club Bd"
-        //     },
-        //     // Add more game history entries here
-        // ];  
-
-        console.info('completeWithdrawal  => ', req.query);
+        console.info('rummyHistory  => ', req.query);
         if (req.query.userId == undefined) {
             res.json({ BlackandWhiteData: [] });
             return false
@@ -75,7 +32,7 @@ router.get('/BackandWhiteHistory', async (req, res) => {
             { DateTime: 1, userId: 1, Name: 1, PhoneNumber: 1, RoomId: 1, Amount: 1, Type: 1, game:1 }).sort({ DateTime: -1 })
 
 
-        console.log("completeWithdrawalData ", BlackandWhiteData)
+        console.log("rummyHistory ", BlackandWhiteData)
 
 
         res.json({ BlackandWhiteData });
@@ -87,78 +44,6 @@ router.get('/BackandWhiteHistory', async (req, res) => {
 });
 
 
-/**
-* @api {get} /admin/aviatorHistory
-* @apiName  add-bet-list
-* @apiGroup  Admin
-* @apiHeader {String}  x-access-token Admin's unique access-key
-* @apiSuccess (Success 200) {Array} badges Array of badges document
-* @apiError (Error 4xx) {String} message Validation or error message.
-*/
-router.get('/aviatorHistory', async (req, res) => {
-    try {
-       
-        // const aviatorHistoryData =  [
-        //     {
-        //         "SrNo": 1,
-        //         "DateTime": "2023-10-10 08:30 AM",
-        //         "Name": "Alice",
-        //         "PhoneNumber": "123-456-7890",
-        //         "RoomId": "RHRoom1",
-        //         "Amount": 100, // Amount in this example (can be credit or debit)
-        //         "Type": "Credit", // "Credit" or "Debit"
-        //         "Club": "Club A"
-        //     },
-        //     {
-        //         "SrNo": 2,
-        //         "DateTime": "2023-10-09 10:15 AM",
-        //         "Name": "Bob",
-        //         "PhoneNumber": "987-654-3210",
-        //         "RoomId": "RHRoom2",
-        //         "Amount": 50, // Amount in this example (can be credit or debit)
-        //         "Type": "Debit", // "Credit" or "Debit"
-        //         "Club": "Club B"
-        //     },
-        //     {
-        //         "SrNo": 3,
-        //         "DateTime": "2023-10-09 10:15 AM",
-        //         "Name": "Bob",
-        //         "PhoneNumber": "987-654-3210",
-        //         "RoomId": "RHRoom2",
-        //         "Amount": 50, // Amount in this example (can be credit or debit)
-        //         "Type": "Debit", // "Credit" or "Debit"
-        //         "Club": "Club Bd"
-        //     }, {
-        //         "SrNo": 3,
-        //         "DateTime": "2023-10-09 10:15 AM",
-        //         "Name": "Bob",
-        //         "PhoneNumber": "987-654-3210",
-        //         "RoomId": "RHRoom2",
-        //         "Amount": 50, // Amount in this example (can be credit or debit)
-        //         "Type": "Debit", // "Credit" or "Debit"
-        //         "Club": "Club Bd"
-        //     },
-        //     // Add more game history entries here
-        // ];
-
-        console.info('aviatorHistoryData  => ', req.query);
-        if (req.query.userId == undefined) {
-            res.json({ aviatorHistoryData: [] });
-            return false
-        }
-        const aviatorHistoryData = await GameHistory.find({ userId: MongoID(req.query.userId), "game": "aviator" },
-            { DateTime: 1, userId: 1, Name: 1, PhoneNumber: 1, RoomId: 1, Amount: 1, Type: 1, game:1 }).sort({ DateTime: -1 })
-
-        console.log("aviatorHistoryData ", aviatorHistoryData)
-
-        logger.info('admin/dahboard.js post dahboard  error => ', aviatorHistoryData);
-
-        res.json({ aviatorHistoryData });
-    } catch (error) {
-        logger.error('admin/dahboard.js post bet-list error => ', error);
-        res.status(config.INTERNAL_SERVER_ERROR).json(error);
-    }
-});
 
 
 /**
@@ -176,8 +61,8 @@ router.get('/completeWithdrawal', async (req, res) => {
             res.json({ completeWithdrawalData: [] });
             return false
         }
-        const completeWithdrawalData = await UserWalletTracks.find({ userId: MongoID(req.query.userId), "trnxTypeTxt": "Withdrawal" },
-            { createdAt: 1, userId: 1, uniqueId: 1, oppWinningChips: 1, trnxAmount: 1, totalBucket: 1, trnxTypeTxt: 1 }).sort({ createdAt: -1 })
+        const completeWithdrawalData = await UserWalletTracks.find({ userId: MongoID(req.query.userId),transTypeText:"PayOut" },
+        { createdAt: 1, userId: 1, uniqueId: 1, chips: 1, transAmount: 1, totalBucket: 1, transTypeText: 1 }).sort({ createdAt: -1 })
 
         console.log("completeWithdrawalData ", completeWithdrawalData)
 
@@ -207,9 +92,8 @@ router.get('/completeDeposite', async (req, res) => {
             return false
         }
 
-        const completeDepositeData = await UserWalletTracks.find({ userId: MongoID(req.query.userId), "trnxTypeTxt": "Deposit" },
-            { createdAt: 1, userId: 1, uniqueId: 1, oppChips: 1, trnxAmount: 1, totalBucket: 1, trnxTypeTxt: 1 }).sort({ createdAt: -1 })
-
+        const completeDepositeData = await UserWalletTracks.find({ userId: MongoID(req.query.userId) , "trnxTypeTxt": "PayIn"},
+        { createdAt: 1, userId: 1, uniqueId: 1, chips: 1, transAmount: 1, totalBucket: 1, transTypeText: 1  }).sort({ createdAt: -1 })
 
         logger.info('admin/dahboard.js post dahboard  error => ', completeDepositeData);
 
@@ -233,44 +117,18 @@ router.get('/registerRaferralBonus', async (req, res) => {
     try {
         //console.info('requet => ', req);
 
-        const registerRaferralBonusData = [
-            {
-                "SrNo": 1,
-                "DateTime": "2023-10-10 08:30 AM",
-                "Name": "Alice",
-                "PhoneNumber": "123-456-7890",
-                "RoomId": "RRRoom1",
-                "Amount": 100, // Amount in this example (can be credit or debit)
-                "Type": "Credit", // "Credit" or "Debit"
-                "Club": "Club A"
-            },
-            {
-                "SrNo": 2,
-                "DateTime": "2023-10-09 10:15 AM",
-                "Name": "Bob",
-                "PhoneNumber": "987-654-3210",
-                "RoomId": "RRRoom2",
-                "Amount": 50, // Amount in this example (can be credit or debit)
-                "Type": "Debit", // "Credit" or "Debit"
-                "Club": "Club B"
-            },
-            {
-                "SrNo": 3,
-                "DateTime": "2023-10-09 10:15 AM",
-                "Name": "Bob",
-                "PhoneNumber": "987-654-3210",
-                "RoomId": "RRRoom2",
-                "Amount": 50, // Amount in this example (can be credit or debit)
-                "Type": "Debit", // "Credit" or "Debit"
-                "Club": "Club Bd"
-            },
-            // Add more game history entries here
-        ];
+        if (req.query.userId == undefined) {
+            res.json({ registerRaferralBonusData: [] });
+            return false
+        }
 
+        const registerRaferralBonusData = await UserWalletTracks.find({ userId: MongoID(req.query.userId) , "trnxTypeTxt": "PayIn"},
+        { createdAt: 1, userId: 1, uniqueId: 1, chips: 1, transAmount: 1, totalBucket: 1, transTypeText: 1  }).sort({ createdAt: -1 })
 
-        logger.info('admin/dahboard.js post dahboard  error => ', registerRaferralBonusData);
+        logger.info('admin/dahboard.js post registerRaferralBonusData  error => ', registerRaferralBonusData);
 
         res.json({ registerRaferralBonusData });
+
     } catch (error) {
         logger.error('admin/dahboard.js post bet-list error => ', error);
         res.status(config.INTERNAL_SERVER_ERROR).json(error);
