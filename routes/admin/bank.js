04c8350c07536fa4d93279702ba3232fd955ff51
ankit <iankitkhatrani@gmail.com>
2024-03-28 16:53:58 +0530

    add

--- routes/admin/bank.js
+++ routes/admin/bank.js
@@ -32,13 +32,52 @@ router.get('/BankList', async (req, res) => {
             wh = { paymentStatus: "Rejected" }
         }
 
-        const bankList = await BankDetails.find(wh, { name: 1, email: 1, phone: 1, accountNumber: 1, IFSC: 1, createdAt: 1, userId: 1, BeneficiaryName: 1, paymentStatus: 1 })
+        const bankList = await BankDetails.find(wh, { name: 1, email: 1,reMark:1, phone: 1, accountNumber: 1, IFSC: 1, createdAt: 1, userId: 1, BeneficiaryName: 1, paymentStatus: 1 })
 
         logger.info('admin/dahboard.js post BankList  error => ', bankList);
 
         res.json({ bankList });
     } catch (error) {
         logger.error('admin/dahboard.js post BankList error => ', error);
+        res.status(config.INTERNAL_SERVER_ERROR).json(error);
+    }
+});
+
+/**
+* @api {post} /admin/AddUser
+* @apiName  add-bet-list
+* @apiGroup  Admin
+* @apiHeader {String}  x-access-token Admin's unique access-key
+* @apiSuccess (Success 200) {Array} badges Array of badges document
+* @apiError (Error 4xx) {String} message Validation or error message.
+*/
+router.put('/BankUpdate', async (req, res) => {
+    try {
+
+        console.log("req ", req.body)
+        if(req.body.reMark == null || req.body.paymentStatus == null){
+            res.json({ status: false });
+            return false
+        }
+        //currently send rendom number and generate 
+        let response = {
+            $set: {
+                reMark: req.body.reMark,
+                paymentStatus:req.body.paymentStatus
+            }
+        }
+
+
+
+        const userInfo = await BankDetails.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(req.body.Id) }, response, { new: true });
+
+        logger.info('admin/dahboard.js post dahboard  error => ', userInfo);
+
+        res.json({ status: "ok" });
+    } catch (error) {
+        logger.error('admin/dahboard.js post bet-list error => ', error);
+        //res.send("error");
+
         res.status(config.INTERNAL_SERVER_ERROR).json(error);
     }
 });
