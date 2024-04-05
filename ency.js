1f779391d0ce096b9e688c3fc323f162841313a7
AnilNikam <anilnikam619@gmail.com>
2024-04-05 13:56:49 +0530

    check new URL whitlisting

--- ency.js
+++ ency.js
@@ -55,7 +55,7 @@ const encryptedData = encrypt(yourData, yourSecretKey, yourInitializationVector)
 console.log('Encrypted data:', encryptedData);
 
 // JSON data to be decrypted
-const receivedEncryptedData = encryptedData; // Replace with the actual encrypted data you received
+const receivedEncryptedData = 'eQdPkWu98+e4uTG9+Cu3Gzw0MlirMCLLpIL/0nB5qaaFmpz0woU2xjctn/Hmpv9L1UJMSlJ7j4cwVLigRp/SvpN026x/UDyvgrZvHk/86pd71LVR/ZDp4EZxW0Cdd5fLWnRFj1qLYLetwc/fQKpqX7u0/nWcUlXMHXLxaExBRPo6SFOL3MJK9CBtj9p8KgFDUA2iqsiw15qfIABhlKf7RQcDSbz0ueBq8utxqG3G3u/BMiMOkhKsrKZTHbd4xjYij1r8IT42+60aUNgBrqY3xQxrhRQwRY3YZi3vrfrErF/JsQeQ+yKgzB7UtQp2B+xZczteJ8F45wtkEHs0fUi7xBiR9z5pnmE4j/4xesqxocBbnf4nDSUHjzNrxz6S1r3J5DcF0fP2zgsfYxH18Kl5xqesKSryYiSGw/cxQ3j+LHBbpAEKKnLBKZyAz+RJZJifnJ7H2+KX5l9bhLmVio0yVSJ4lTNZjIjG94HK2UFg97o7KYInMg9f689ed160jTrpEiDwwTWPQwHhgsmTC0VaWyDFHxFOdbI80cDpNYxq1as0QjjXtlDiMLkgXUQ+RIEHl4Pb0fxaQeiR0zcandga5A=='; // Replace with the actual encrypted data you received
 
 const decryptedData = decrypt(receivedEncryptedData, yourSecretKey, yourInitializationVector);
 console.log('Decrypted data:', decryptedData);
